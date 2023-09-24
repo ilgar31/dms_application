a3ec5ef2ca5f9144e48ac6e20854e84e6cbde058
ilgar31 <ilgar.bagishev@gmail.com>
2023-09-24 17:50:44 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  MyApp({Key key}): super(key: key);
+
+  @override
+  _MyApp createState() => _MyApp();
+}
+
+class _MyApp extends State {
+
+  var _currentPage = 0;
+}
